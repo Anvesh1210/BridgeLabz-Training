986cfba9a3e62f0384eddd21a6bba740503577d8
Anvesh <anveshsahu15@gmail.com>
2025-12-16 16:50:52 +0530

    [Anvesh] : added Addition of Two Numbers

---
+++ gcr-codebase/program-prerequisites/AddTwoNum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class AddTwoNum{
+	public static void main(String[] args){
+		Scanner kb=new Scanner(System.in);
+		int a=kb.nextInt();
+		int b=kb.nextInt();
+		int sum=a+b;
+		System.out.println(sum);
+	}
+}
+	
