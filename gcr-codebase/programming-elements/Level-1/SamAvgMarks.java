b9277aa4ed5878ed5018433c614e86a25005afe0
Anvesh <anveshsahu15@gmail.com>
2025-12-17 23:14:03 +0530

    [Anvesh] : Added Sam Avergae Marks

---
+++ gcr-codebase/programming-elements/Level-1/SamAvgMarks.java
@@ -0,0 +1,20 @@
+//Finding Sam Average Marks
+public class SamAvgMarks
+{
+	public static void main(String[] args)
+	{
+
+		//Giving PCM marks
+		int mathMarks = 94;
+		int physicsMarks = 95;
+		int chemistryMarks = 96;
+
+		//Calculating Average Marks
+		double averageMarks = (mathMarks + physicsMarks + chemistryMarks)/3.0;
+
+		//Printing Sam's Average Marks
+		System.out.println("Sam average mark in PCM is "+averageMarks);
+	}
+}
+
+	
