880416035a0af7ca03015a470a1100c3089a7cfd
Anvesh <anveshsahu15@gmail.com>
2025-12-17 23:24:57 +0530

    [Anvesh] : Added Kilometers to miles

---
+++ gcr-codebase/programming-elements/Level-1/KmToMiles.java
@@ -0,0 +1,16 @@
+//Converting Kilometers to Miles
+public class KmToMiles
+{
+	public static void main(String[] args)
+	{
+		
+		//Giving Input
+		double distanceInKm = 10.8;
+
+		//Calculating Distance in Miles
+		double distanceInMiles = distanceInKm/1.6;
+
+		//Displaying details
+		System.out.println("The distance "+distanceInKm+" km in miles is "+distanceInMiles);
+	}
+}
