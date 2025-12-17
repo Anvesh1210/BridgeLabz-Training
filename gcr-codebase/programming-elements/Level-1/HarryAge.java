6e1d52d06fc96e34ce19c2d330bcc7b82f78b8a1
Anvesh <anveshsahu15@gmail.com>
2025-12-17 22:13:27 +0530

    [Anvesh] : Added Harry Age Program

---
+++ gcr-codebase/programming-elements/Level-1/HarryAge.java
@@ -0,0 +1,17 @@
+//Finding Harry Current Age
+public class HarryAge
+{
+	public static void main(String []args)
+	{
+		
+		//Giving Input
+		int birthyear = 2000;
+		int currentYear = 2024;
+
+		//Calculating Harry current Age
+		int currentAge = currentYear- birthyear;
+
+		//Displaying Harry current Age
+		System.out.println("Harry's current age is: "+currentAge);
+	}
+}
