2fc9d46f693ac374666a7bff6e4a7bbcf10f5afd
Anvesh <anveshsahu15@gmail.com>
2025-12-17 23:32:01 +0530

    [Anvesh] : Adding distribution of pens

---
+++ gcr-codebase/programming-elements/Level-1/DistributePens.java
@@ -0,0 +1,19 @@
+//Finding Distrubation of Pens
+public class DistributePens
+{
+	public static void main(String []args)
+	{
+		
+		//giving Input Details
+		int totalNoOfPens = 14;
+
+		//Calculating Pens per Student and Remaning Pen
+		int pensPerStudent = 14/3;
+		int remaningPens = 14%3;
+
+		//Displaying Details
+		System.out.println("The Pen Per Student is "+pensPerStudent+" and the remaining pen not distributed is "+remaningPens);
+	}
+}
+		+
+		
