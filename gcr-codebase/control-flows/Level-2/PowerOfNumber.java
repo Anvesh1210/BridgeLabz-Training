eb535eb2a6e90e17981362d26678a46a42b6f929
Anvesh <anveshsahu15@gmail.com>
2025-12-19 12:36:08 +0530

    [Anvesh] : Added Power of a given number program

---
+++ gcr-codebase/control-flows/Level-2/PowerOfNumber.java
@@ -0,0 +1,29 @@
+//Finding The power of given number
+import java.util.Scanner;
+class PowerOfNumber{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		//Taking a number and its power from the user 
+		System.out.print("Input a number: ");
+		int number = input.nextInt();
+		System.out.print("Input it's Power: ");
+		int power = input.nextInt();
+		
+		//finding the power of number
+		if(number>0 && power>0){
+			int result = 1;
+			for(int i=1;i<=power;i++){
+				result*=number;
+			}
+			
+			//printing the power of number
+			System.out.println("Result accroding to number and its power: "+result);
+		}else{
+			System.out.println("Not A Positive Number or grater then 100");
+		}
+		
+		//closing the Scanner
+		input.close();
+	}
+}
