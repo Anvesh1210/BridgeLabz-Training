ccc4a9808a3bd64bdbf231a9b975e408ff6bcdfc
Anvesh <anveshsahu15@gmail.com>
2026-01-28 10:34:15 +0530

    [Anvesh] : Added bad words censoring using regex

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/regex/basic/CensorBadWords.java
@@ -0,0 +1,10 @@
+package com.regex.basic;
+
+public class CensorBadWords {
+	public static void main(String[] args) {
+		String input = "This is a damn bad example with some stupid words.";
+		String regex = "\\b(damn|stupid)\\b";
+		String output = input.replaceAll(regex, "****");
+		System.out.println(output);
+	}
+}
