4d313b57f6feae5bf3aa8c69a676a60b3dbb1652
Anvesh <anveshsahu15@gmail.com>
2026-02-05 01:41:42 +0530

    [Anvesh] : Added Validate JSON

---
+++ java-io-streams-practice/gcr-codebase/JavaIOStreamConcepts/src/com/json/validatejson/ValidateJson.java
@@ -0,0 +1,16 @@
+package com.json.validatejson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ValidateJson {
+	public static void main(String[] args) {
+		String json = "{ \"name\":\"Anvesh\", \"age\":22 }";
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.readTree(json);
+			System.out.println("Valid JSON");
+		} catch (Exception e) {
+			System.out.println("Invalid JSON");
+		}
+	}
+}
